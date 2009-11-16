6ec6678ff2af1362da9495a7fbb12b5a60ec9452
jugyo <jugyo.org@gmail.com>
2009-11-16 17:17:12 +0900

    String に変換してから検証を行うように変えた

--- lib/validates_for_convert_encoding.rb
+++ lib/validates_for_convert_encoding.rb
@@ -16,8 +16,9 @@ module ConvertEncodingValidation
       columns_for_convert_encode.each do |key, options|
         convert_encodeing_to = options[:to].to_s
         convert_encodeing_from = options[:from].to_s
+        convert_value = self.attributes[key].to_s
         begin
-          Iconv.new(convert_encodeing_to, convert_encodeing_from).iconv(self.attributes[key])
+          Iconv.new(convert_encodeing_to, convert_encodeing_from).iconv(convert_value)
         rescue Iconv::IllegalSequence => e
           errors.add(key, "invalid sequence for #{convert_encodeing_to} from #{convert_encodeing_from}")
         end
